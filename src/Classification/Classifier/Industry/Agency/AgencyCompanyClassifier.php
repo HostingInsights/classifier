0c6b4f39f521738c8fc7a3be0e908e101cd86103
Nils Langner <nils.langner@startwind.io>
2024-07-14 16:20:39 +0200

    agentur

--- src/Classification/Classifier/Industry/Agency/AgencyCompanyClassifier.php
+++ src/Classification/Classifier/Industry/Agency/AgencyCompanyClassifier.php
@@ -10,8 +10,26 @@ class AgencyCompanyClassifier implements Classifier
     public const CLASSIFIER_PREFIX = 'industry:agency';
 
     private $keywords = [
-        'agentur',
-        'agency'
+        "agentur",
+        "agency",
+        "agencia",
+        "agence",
+        "agenzia",
+        "agência",
+        "Агентство",
+        "机构 / 代理机构",
+        "代理店",
+        "대리점",
+        "وكالة",
+        "Ajans",
+        "एजेंसी",
+        "এজেন্সি",
+        "agentschap",
+        "Πρακτορείο",
+        "Byrå",
+        "agencja",
+        "Ügynökség",
+        "agenție"
     ];
 
     public function classify(HttpResponse $httpResponse, array $existingTags): array
@@ -20,6 +38,8 @@ class AgencyCompanyClassifier implements Classifier
         $metaDescription = $httpResponse->getHtmlDocument()->getMetaDescription();
 
         foreach ($this->keywords as $keyword) {
+            $keyword = strtolower($keyword);
+
             if (str_contains(strtolower($title), $keyword)
                 || str_contains(strtolower($metaDescription), $keyword)) {
                 return [self::CLASSIFIER_PREFIX];
